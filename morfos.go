1c0bb56b341697b301b579213eff3285d85359aa
Tiago Katcipis <tiagokatcipis@gmail.com>
2017-04-30 18:15:04 -0300

    Remove unused code

--- morfos.go
+++ morfos.go
@@ -2,38 +2,6 @@ package morfos
 
 import "unsafe"
 
-// tflag values must be kept in sync with copies in:
-//	cmd/compile/internal/gc/reflect.go
-//	cmd/link/internal/ld/decodesym.go
-//	runtime/type.go
-type tflag uint8
-
-type typeAlg struct {
-	// function for hashing objects of this type
-	// (ptr to object, seed) -> hash
-	hash func(unsafe.Pointer, uintptr) uintptr
-	// function for comparing objects of this type
-	// (ptr to object A, ptr to object B) -> ==?
-	equal func(unsafe.Pointer, unsafe.Pointer) bool
-}
-
-type nameOff int32 // offset to a name
-type typeOff int32 // offset to an *rtype
-
-type rtype struct {
-	size       uintptr
-	ptrdata    uintptr
-	hash       uint32   // hash of type; avoids computation in hash tables
-	tflag      tflag    // extra type information flags
-	align      uint8    // alignment of variable with this type
-	fieldAlign uint8    // alignment of struct field with this type
-	kind       uint8    // enumeration for C
-	alg        *typeAlg // algorithm table
-	gcdata     *byte    // garbage collection data
-	str        nameOff  // string form
-	ptrToThis  typeOff  // type for pointer to this type, may be zero
-}
-
 type eface struct {
 	Type unsafe.Pointer
 	Word unsafe.Pointer
