b27e809a37f9841eeb6cbf947e9190c2daecf3d3
Tiago Katcipis <tiagokatcipis@gmail.com>
2017-04-30 19:52:10 -0300

    Add mutable string test

--- morfos_test.go
+++ morfos_test.go
@@ -2,6 +2,7 @@ package morfos_test
 
 import (
 	"testing"
+	"unsafe"
 
 	"github.com/katcipis/morfos"
 )
@@ -35,5 +36,44 @@ func TestStructsSameSize(t *testing.T) {
 
 	if orig.y != morphedNotOriginal.w {
 		t.Fatalf("expected y[%d] == w[%d]", orig.y, morphedNotOriginal.w)
+	}
+}
+
+func TestMutatingString(t *testing.T) {
+
+	type stringStruct struct {
+		str unsafe.Pointer
+		len int
+	}
+
+	var rawstr [5]byte
+	rawstr[0] = 'h'
+	rawstr[1] = 'e'
+	rawstr[2] = 'l'
+	rawstr[3] = 'l'
+	rawstr[4] = 'o'
+
+	hi := stringStruct{
+		str: unsafe.Pointer(&rawstr),
+		len: len(rawstr),
+	}
+
+	somestr := ""
+
+	morphed := morfos.Morph(hi, somestr)
+	mutableStr := morphed.(string)
+
+	if mutableStr != "hello" {
+		t.Fatalf("expected hello, got: %s", mutableStr)
+	}
+
+	rawstr[0] = 'h'
+	rawstr[1] = 'a'
+	rawstr[2] = 'c'
+	rawstr[3] = 'k'
+	rawstr[4] = 'd'
+
+	if mutableStr != "hackd" {
+		t.Fatalf("expected hackd, got: %s", mutableStr)
 	}
 }
